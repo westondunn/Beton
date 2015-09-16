079caf7534d537345644bfaccf90737e6824e208
Avner Gershtansky <avnerg@perfectomobile.com>
2015-09-16 11:27:44 +0300

    Removed "file busy" notification between retries.

--- Beton/src/main/java/com/perfectomobile/dataDrivers/Lock.java
+++ Beton/src/main/java/com/perfectomobile/dataDrivers/Lock.java
@@ -9,13 +9,13 @@ public class Lock{
 	}
 
 	public synchronized void lock() throws Exception{
-		int waitCycles = 10;
+		int waitCycles = 20;
 		int waited = 0;
 		while(isLocked){
 			wait(10000);
 			if(isLocked){
 				waited++;
-				System.out.println("File is locked, waiting...");
+				//System.out.println("File is locked, waiting...");
 			}
 			if(waited == waitCycles){
 				throw new Exception("Lock wait time exceeded");
